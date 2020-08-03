acbdabd45ef4c00fe17a17ec4d7a80f7500957b3
Ariba Khan <akhan41@binghamton.edu>
2020-08-03 22:02:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ myfavdesserts = ["chocolate cake", "cupcakes", "oreos", "twix" ]
 end
 
 def add_element_to_end_of_array(array, element)
-
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
